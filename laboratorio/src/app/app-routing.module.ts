178d2306755cab74b2b7aa6d8b0acf7433a3c921
Jefferson Pedro <jeffersonpedro05@gmail.com>
2023-09-10 14:04:11 -0300

    update
    Tentativa de criar um dashboard

--- laboratorio/src/app/app-routing.module.ts
+++ laboratorio/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { DashboardComponent } from './dashboard-2/dashboard.component';
+import { DashboardMaterialComponent } from './dashboard-material/dashboard-material.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, 
@@ -9,6 +11,10 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
 
   {path: 'calendar', component: CalendarComponent},
+
+  {path: 'relatorio', component: DashboardMaterialComponent},
+
+
 
 ];
 
--- laboratorio/src/app/app.module.ts
+++ laboratorio/src/app/app.module.ts
@@ -7,12 +7,21 @@ import { CalendarModule } from './calendar/calendar.module';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard-2/dashboard.component';
+import { DashboardMaterialComponent } from './dashboard-material/dashboard-material.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DashboardComponent,
+    DashboardMaterialComponent
 
   ],
   imports: [
@@ -20,7 +29,12 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     CalendarModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
